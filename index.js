9db16b42d45a728906eb89c2edefde63e3efe9fa
juelhossain <juelhossainnn@gmail.com>
2022-05-12 20:30:49 +0600

    problem fixed

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 5000;
-const { MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
 // connect database
 //-------------------------
 const uri =
-  "mongodb+srv://dbuserjuel:Iiply5I7uHFZ7HtH@cluster0.o2iwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o2iwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,64 +20,61 @@ const client = new MongoClient(uri, {
 // insert data to database
 //------------------------
 const run = async () => {
-    try {
+  try {
+    await client.connect();
+    const fruit = client.db("eFruits-Management").collection("fruits");
 
-        await client.connect();
-        const fruit = client.db("eFruits-Management").collection('fruits');
+    app.get("/fruits", async (req, res) => {
+      const query = {};
+      const cursor = fruit.find(query);
+      const fruits = await cursor.toArray();
+      res.send(fruits);
+    });
+    app.get("/fruits/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await fruit.findOne(query);
+      res.send(result);
+    });
+    app.post("/fruits", async (req, res) => {
+      const newFruit = req.body;
+      const result = await fruit.insertOne(newFruit);
+      res.send(result);
+      console.log(newFruit, "added successfully");
+    });
+    // updating user
+    app.put("/fruits/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedFruit = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: updatedFruit,
+      };
+      const result = await fruit.updateOne(filter, updatedDoc, options);
+      res.send(result);
+      console.log(fruit, "updated");
+    });
+    //delete a fruitk
+    app.delete("/fruits/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await fruit.deleteOne(query);
+      res.send(result);
+      console.log(fruit, "deleted");
+    });
+  } finally {
+    console.log("Everythin is fine add an item.");
+  }
+};
+run().catch(console.log);
 
-        app.get('/fruits', async (req, res) => {
-            const query = {};
-            const cursor = fruit.find(query);
-            const fruits = await cursor.toArray();
-            res.send(fruits);
-        })
-        app.get('/fruits/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await fruit.findOne(query);
-            res.send(result);
-        })
-        app.post('/fruits', async (req, res) => {
-            const newFruit = req.body;
-            const result = await fruit.insertOne(newFruit);
-            res.send(result);
-            console.log(newFruit, 'added successfully');
-        });
-        // updating user 
-        app.put('/fruits/:id', async (req, res) => {
-            const id = req.params.id;
-            const updatedFruit = req.body;
-            const filter = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: updatedFruit
-            };
-            const result = await fruit.updateOne(filter, updatedDoc, options);
-            res.send(result);
-            console.log(fruit,'updated')
-        })
-        //delete a fruitk
-        app.delete('/fruits/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = {_id: ObjectId(id) };
-            const result = await fruit.deleteOne(query);
-            res.send(result);
-            console.log(fruit,'deleted')
-        })
-    }
-    finally {
-        console.log('Everythin is fine add an item.');
-    }
-}
-run().catch(console.log)
-
-// running the server 
+// running the server
 //-------------------------------------
-app.get('/', (req, res) => {
-    res.send(" Server is running");
+app.get("/", (req, res) => {
+  res.send(" Server is running");
 });
 
 app.listen(port, () => {
-    console.log('server is running on', port)
+  console.log("server is running on", port);
 });
-
