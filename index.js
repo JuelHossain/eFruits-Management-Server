30859f33acd9060733237ecaae247ec0992ea8c4
juelhossain <juelhossainnn@gmail.com>
2022-05-13 14:40:26 +0600

    cannot identify problems

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
+// cors are okey then what is the problem?
 
 // connect database
 //-------------------------
