2051060fbabfe3f66114bcd295bc2cb9a08f986c
juelhossain <juelhossainnn@gmail.com>
2022-05-16 11:19:42 +0600

    comment updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 // cors are okey then what is the problem?
 
-// connect database
+// connecting database
 //-------------------------
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o2iwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -24,7 +24,8 @@ const run = async () => {
   try {
     await client.connect();
     const fruit = client.db("eFruits-Management").collection("fruits");
-
+ 
+    // getting all fruits 
     app.get("/fruits", async (req, res) => {
       const page = parseInt(req.query.page);
       const size = parseInt(req.query.size);
@@ -38,25 +39,30 @@ const run = async () => {
       }
       res.send(fruits);
     });
+
+    // getting all fruit count
     app.get('/fruitsCount', async (req, res) => {
       const query = {};
       const cursor = fruit.find(query);
       const count = await fruit.countDocuments();
       res.send({ count });
     })
+    // getting one fruit with id 
     app.get("/fruits/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await fruit.findOne(query);
       res.send(result);
     });
+
+    // adding fruit to the data base 
     app.post("/fruits", async (req, res) => {
       const newFruit = req.body;
       const result = await fruit.insertOne(newFruit);
       res.send(result);
       console.log(newFruit, "added successfully");
     });
-    // updating user
+    // updating fruits to the data base 
     app.put("/fruits/:id", async (req, res) => {
       const id = req.params.id;
       const updatedFruit = req.body;
@@ -69,7 +75,7 @@ const run = async () => {
       res.send(result);
       console.log(fruit.name, "updated");
     });
-    //delete a fruitk
+    //deleting single fruit from data base 
     app.delete("/fruits/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
@@ -78,7 +84,7 @@ const run = async () => {
       console.log(fruit, "deleted");
     });
   } finally {
-    console.log("Everythin is fine add an item.");
+    console.log("Everythin is fine.");
   }
 };
 run().catch(console.log);
