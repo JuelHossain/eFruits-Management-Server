fa225be6900a550ed02afacfa6f6f51d19b2926d
juelhossain <juelhossainnn@gmail.com>
2022-10-24 03:28:22 +0600

    updated mongodb

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 const verifyJwt = require("./middlewares/verifyJwt");
 const client = require("./db/client");
 const fruitsRouter = require("./routers/fruitsRouter");
