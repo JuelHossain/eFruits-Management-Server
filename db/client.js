e9b7858e1bbb7dcfe8baf12593b9f380bd042e57
juelhossain <juelhossainnn@gmail.com>
2022-10-23 21:59:41 +0600

    env modified

--- db/client.js
+++ db/client.js
@@ -1,12 +1,9 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb")
 
-const uri = "mongodb+srv://cluster0.o2iwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASS,
+const client = new MongoClient(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1
-});
+  serverApi: ServerApiVersion.v1,
+})
 
-module.exports = client;
+module.exports = client
