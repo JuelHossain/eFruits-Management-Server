c351e21887a83f39fa6f5801bf73a58134f57c43
juelhossain <juelhossainnn@gmail.com>
2022-10-23 23:20:20 +0600

    hey

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const client = new MongoClient(process.env.DATABASE_URL, {
+const client = new MongoClient(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1
