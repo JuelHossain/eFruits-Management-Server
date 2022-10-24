d30a674f744ab8c275a53ab4ef11686f29497d6e
juelhossain <juelhossainnn@gmail.com>
2022-10-24 10:02:12 +0600

    jwt set to 1h

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -6,7 +6,7 @@ const loginRouter = express.Router();
 loginRouter.post("/", async (req, res) => {
   const user = req.body;
   const accessToken = await jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "1m"
+    expiresIn: "1h"
   });
   res.send({ accessToken });
 });
